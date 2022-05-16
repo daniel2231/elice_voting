310b4fb151ca6cb4905dfa0eab823e920d9db448
Daniel2231 <daniel2231.dev@gmail.com>
2022-05-16 20:09:56 +0900

    added pages and server setup

--- server.js
+++ server.js
@@ -1,9 +1,26 @@
 // express js server
-require('dotenv').config()
-const express = require('express')
-const mongoose = require('mongoose')
-const path = require('path')
-const {MONGO_URI, PORT} = process.env
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const path = require("path");
+const { MONGO_URI, PORT } = process.env;
 
+mongoose
+  .connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.log(err));
 
-const app = express()
+const app = express();
+
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/views/pages");
+
+// 라우터 설정
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
